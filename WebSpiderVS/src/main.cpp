003435796768064bc4754b78d98e6998f82b6062
Thomas <tczernik.mmt-b2008@fh-salzburg.ac.at>
2010-03-18 13:49:20 +0100

    Modified gitignore file

--- WebSpiderVS/src/main.cpp
+++ WebSpiderVS/src/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
 int main() {
+
+	ShellExecute(Null, "open", "http://www.google.de", NULL, NULL, SW_SHOWNORMAL);
+  return EXIT_SUCCESS;
 
 	return 0;
 
