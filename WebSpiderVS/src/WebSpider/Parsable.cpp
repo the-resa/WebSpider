4a708bc628d13bdf4a1a8ce0f0eaf7e31597252b
Matthias Frick <mfrick.mmt-b2008@fh-salzburg.ac.at>
2010-04-08 11:33:06 +0200

    worked at parsable.cpp - function to parse a string

--- WebSpiderVS/src/WebSpider/Parsable.cpp
+++ WebSpiderVS/src/WebSpider/Parsable.cpp
@@ -1,2 +1,8 @@
 # include "Parsable.h"
 
+
+std::vector<std::string> Parsable::parse(std::string stringToParse) {
+	vector<std::string> parseResult;
+	boost::algorithm::split_regex(parseResult, stringToParse, regex( "" /* hier regex rein */ ) ) ;
+	return parseResult;
+}
--- WebSpiderVS/src/WebSpider/Parsable.h
+++ WebSpiderVS/src/WebSpider/Parsable.h
@@ -2,11 +2,18 @@
 #define PARSABLE_H
 
 #include <boost/regex.hpp>
+#include <boost/algorithm/string/regex.hpp>
+#include <string>
+#include <vector>
 
 class Parsable {
 
 public:
+	std::vector<std::string> parse(std::string);
 	std::string stringToParse;
+
+private:
+	
 
 };
 
