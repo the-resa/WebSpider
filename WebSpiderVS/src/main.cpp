ead929df352659a9aa71125aca06afd4de65e430
Ivo Havranek <killfrogg@gmx.at>
2010-04-13 20:08:56 +0200

    fixed merge confilcts

--- WebSpiderVS/src/main.cpp
+++ WebSpiderVS/src/main.cpp
@@ -2,14 +2,8 @@
 
 int main()
 {
-<<<<<<< HEAD
-	WebSpider spider;
-	spider.crawlURL("www.fh-salzburg.ac.at");
-=======
-
 	WebSpider spider("http", "www.frick-web.at");
 	spider.crawl("/Demosite/");
->>>>>>> 228c7f46d30a1f2cde3c69a3992ecb15284c9454
 
 	system("PAUSE");
 
