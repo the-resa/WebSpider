aceca06bfdcb2399f09650a7e907b9d1d9ceaf04
Thomas <tczernik.mmt-b2008@fh-salzburg.ac.at>
2010-05-04 01:39:53 +0200

    Now it runs on Linux

--- WebSpiderVS/src/WebSpider.cpp
+++ WebSpiderVS/src/WebSpider.cpp
@@ -113,29 +113,24 @@ void WebSpider::crawl(string path, string file) {
 				boost::asio::read_until(socket, response, "\r\n\r\n");
 
 				ostringstream ss;
-				//string data;
+		
 
 				// Read until EOF, writing data to output as we go.
 				while (boost::asio::read(socket, response,
 					boost::asio::transfer_at_least(1), error)) {
-						//  cout << &response;
+
 						ss << &response;
 				}
 				if (error != boost::asio::error::eof)
 					throw boost::system::system_error(error);		
 
 				vector<string> links;
-				//data = ss.str();
 				// TODO: href= can also start with ' instead of " ; nice-to-have: regex filter last path
 				boost::regex e("<\\s*A\\s+[^>]*href\\s*=\\s*\"([^\"]*)\"",boost::regbase::normal | boost::regbase::icase);
-			
-				boost::regex_split(std::back_inserter(links), ss.str(), e);
-				//boost::sregex_token_iterator i(data.begin(), data.end(), e, -1);
-				//boost::sregex_token_iterator j;
-			/*	 while(i != j)
-				{
-					std::cout << *i++ << std::endl << "-------";
-					}*/
+				
+				string data = ss.str();
+				boost::regex_split(std::back_inserter(links), data, e);
+				
 #ifdef MULTITHREADING
 				mutex.lock();
 #endif
